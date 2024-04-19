17821f2766c6d685bdfe0d633bc9abf514c40317
sevilzeynali <sevilzeynali@gmail.com>
2024-04-19 15:37:41 +0200

    ajouter les codes de questions réponses

--- 05-analyze-text/Python/text-analysis/text-analysis.py
+++ 05-analyze-text/Python/text-analysis/text-analysis.py
@@ -28,8 +28,8 @@ def main():
         for file_name in os.listdir(reviews_folder):
             # Read the file contents
             print('\n-------------\n' + file_name)
-            text = open(os.path.join(reviews_folder, file_name), 
-                        encoding='utf8').read()
+            text = open(os.path.join(reviews_folder, file_name),
+                    encoding='utf8').read()
             # print('\n' + text)
             # Get language
             detected_language = ai_client.detect_language(documents=[text])[0]
