3b3a4ccdb27a952ca87275f8082ad1504f4b7a36
sevilzeynali <sevilzeynali@gmail.com>
2024-05-13 10:50:40 +0200

    ajouter les codes de Azure AI services sécurisé

---
+++ 02-ai-services-security/rest-test.sh
@@ -0,0 +1,1 @@
+ curl -X POST "<yourEndpoint>/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 81468b6728294aab99c489664a818197" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
