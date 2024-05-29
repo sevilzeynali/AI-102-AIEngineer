7f42113a1b426853e71db1427e834e6e61cf4252
sevilzeynali <sevilzeynali@gmail.com>
2024-05-29 10:37:44 +0200

    ajouter les codes de analyser des images avec Azure AI Vision

---
+++ 04-use-a-container/rest-test.sh
@@ -0,0 +1,1 @@
+curl -X POST "http://20.238.6.164:5000/text/analytics/v3.0/languages" -H "Content-Type: application/json" --data-ascii "{'documents':[{'id':1,'text':'Hello world.'},{'id':2,'text':'Salut tout le monde.'}]}"
